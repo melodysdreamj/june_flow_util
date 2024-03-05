5c50fe11e2337237525dc6e427fb1eb5ff11b00c
june lee <melodysdreamj@gmail.com>
2024-03-05 15:11:17 +0900

    Update _.dart

--- lib/ready_annotation/_.dart
+++ lib/ready_annotation/_.dart
@@ -1,13 +1,13 @@
 class ReadyApp {
-  final double index;
-  const ReadyApp(this.index);
+  final double? index;
+  const ReadyApp({this.index});
 }
 
 class ReadyRunApp {
-  final double index;
-  const ReadyRunApp(this.index);
+  final double? index;
+  const ReadyRunApp({this.index});
 }
 class ReadyMaterialApp {
-  final double index;
-  const ReadyMaterialApp(this.index);
+  final double? index;
+  const ReadyMaterialApp({this.index});
 }
