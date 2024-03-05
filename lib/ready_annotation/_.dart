dd4038c5b5afa75a659a43d17c8e239210cab1a1
june lee <melodysdreamj@gmail.com>
2024-03-05 14:58:18 +0900

    Create _.dart

---
+++ lib/ready_annotation/_.dart
@@ -0,0 +1,13 @@
+class ReadyApp {
+  final double index;
+  const ReadyApp(this.index);
+}
+
+class ReadyRunApp {
+  final double index;
+  const ReadyRunApp(this.index);
+}
+class ReadyMaterialApp {
+  final double index;
+  const ReadyMaterialApp(this.index);
+}
